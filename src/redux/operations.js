ab857aa7c4884845d7a4673aea0497bb34eb5e06
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-10-09 21:23:24 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,14 +1,32 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { addContact, deleteContact, getContacts } from "api/api";
 
-export const getConstactsThunk = createAsyncThunk('contacts/fetchContactsStatus', async () => {
-    return await getContacts()
+export const getConstactsThunk = createAsyncThunk('contacts/fetchContactsStatus', async (_, thunkAPI) => {
+    try {
+        return await getContacts()
+    }
+    catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+    
 })
 
-export const deleteContactThunk = createAsyncThunk('contacts/deleteContactStatus', async (id) => {
-    return await deleteContact(id)
+export const deleteContactThunk = createAsyncThunk('contacts/deleteContactStatus', async (id, thunkAPI) => {
+    try {
+        return await deleteContact(id)
+    }
+    catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+   
 })
 
-export const addContactThunk = createAsyncThunk('contacts/addContactStatus', async (contact) => {
-    return await addContact(contact)
+export const addContactThunk = createAsyncThunk('contacts/addContactStatus', async (contact, thunkAPI) => {
+    try {
+        return await addContact(contact)  
+    }
+    catch (error) {
+        return thunkAPI.rejectWithValue(error.message); 
+    }
+    
 })
